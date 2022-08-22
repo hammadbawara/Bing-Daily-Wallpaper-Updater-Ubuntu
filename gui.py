309414dbeec015712014523a887a55be4ae876e6
Hammad Zafar Bawara <hammadbawara@gmail.com>
2022-08-22 13:55:16 +0500

    Update Progress Dialog Added

--- gui.py
+++ gui.py
@@ -1,7 +1,9 @@
 import os
-import WallpaperManager
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QThread
+
+import WallpaperManager
 
 CURRENT_WALLPAPER_PATH = f"/home/{os.getlogin()}/.wallpapers/current_wallpaper.txt"
 WALLPAPER_FOLDER_PATH = f"/home/{os.getlogin()}/.wallpapers/"
@@ -30,6 +32,7 @@ class Ui_MainWindow(object):
         self.updateBtn = QtWidgets.QPushButton(self.centralwidget)
         self.updateBtn.setGeometry(QtCore.QRect(300, 509, 161, 51))
         self.updateBtn.setObjectName("updateBtn")
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
@@ -38,15 +41,26 @@ class Ui_MainWindow(object):
         # click listeners
         self.nextBtn.clicked.connect(self.next_wallpaper)
         self.prevBtn.clicked.connect(self.prev_wallpaper)
-        self.updateBtn.clicked.connect(self.update_and_set_wallpaper)
+        self.updateBtn.clicked.connect(self.update_wallpaper_btn_action)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def update_and_set_wallpaper(self):
-        wallpaper = WallpaperManager.download()
-        WallpaperManager.set_wallpaper(wallpaper)
-        self.image_view.setPixmap(QtGui.QPixmap(f'{WALLPAPER_FOLDER_PATH}/{wallpaper}'))
+    def update_wallpaper_btn_action(self):
+        #show progress dialog
+        self.progress_dialog = QtWidgets.QProgressDialog(self.centralwidget)
+        self.progress_dialog.setWindowTitle("Updating Wallpaper")
+        self.progress_dialog.setLabelText("Checking for new wallpaper...")
+        self.progress_dialog.setCancelButton(None)
+        self.progress_dialog.setValue(0)
+        self.progress_dialog.show()
+        self.progress_dialog.autoClose()
+
+        #start thread to update wallpaper
+        self.thread = QThread()
+        self.worker = UpdateWallpaper()
+        self.worker.start()
+
 
     def next_wallpaper(self):
         WallpaperManager.set_next_wallpaper()
@@ -62,6 +76,21 @@ class Ui_MainWindow(object):
         self.nextBtn.setText(_translate("MainWindow", "Next"))
         self.prevBtn.setText(_translate("MainWindow", "Previous"))
         self.updateBtn.setText(_translate("MainWindow", "Update Wallpaper"))
+
+class UpdateWallpaper(QThread):
+    def __init__(self):
+        QThread.__init__(self)
+
+    def run(self):
+        ui.progress_dialog.setValue(10)
+        wallpaper = WallpaperManager.download()
+        ui.progress_dialog.setValue(50)
+        WallpaperManager.set_wallpaper(wallpaper)
+        ui.progress_dialog.setValue(100)
+        ui.image_view.setPixmap(QtGui.QPixmap(f'{WALLPAPER_FOLDER_PATH}/{WallpaperManager.CURRENT_WALLPAPER}'))
+        ui.progress_dialog.close()
+
+
 
 
 if __name__ == "__main__":
